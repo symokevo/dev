dc7b8743da8fe60bd0082a768ab216fbc01f7784
zimaster <symokevo@gmail.com>
2023-04-16 04:59:54 +0300

    cover ruby hashes and their methods

--- ruby/ruby_collections.rb
+++ ruby/ruby_collections.rb
@@ -32,4 +32,62 @@ p teams
 
 # pop - Remove
 z = teams.pop
-p teams
+p teams # removes the last element
+
+# Hash tutorial
+# Hashes - key/value pair collections 
+positions = {
+    first_base: "Simon kevin",
+    second_base: "Jose Altuve",
+    short_stop: "Carlos Correa"
+}
+# another way of using hashes
+positions1 = {"first_base" => "simon kevin", "second_base" => "david berrings", "short_stop" => "augustine junior"}
+# or another way
+positions2 = { :first_base =>"simon kevin", :second_base =>"david berrings", :short_stop =>"maxwel ochi"} 
+
+# select an item from the hash - use key
+p positions[:second_base]
+
+# deleting elements from a hash
+people = { simon: 25, david: 27, junior: 21, maxy: 24}
+p people[:maxy]
+p people.delete(:maxy)
+p people
+
+# iterating over a hash for a key or value
+# to only grab the keys:
+people.each_key do |key|
+    puts key
+end
+
+# to only grab the values: 
+people.each_value do |value|
+    puts value
+end
+
+# Hash methods
+# adding elements
+people[:mike] = 17
+p people
+
+# SWAPPING keys and values
+# 1 - iterate each pair, store in a variable, swap them and update 
+# 2 - use ruby invert 
+people_2 = people.invert
+p people_2
+
+# marging hashes
+people.merge(people_2)
+
+# converting a hash into an array
+p Array(people)
+p people.to_a
+
+# listing all keys
+key_list = people.keys 
+p key_list
+
+# listing all values
+value_list = people.values
+p value_list
