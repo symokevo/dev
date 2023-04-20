2688e496a1dea902a084ca3503d169c788332307
zimaster <symokevo@gmail.com>
2023-04-20 07:59:46 +0300

    cover content on liskov principle

--- ruby/ruby_classes.rb
+++ ruby/ruby_classes.rb
@@ -156,3 +156,61 @@ bill_of_lading = BillingOfLading.new(customer: "Yahoo", total: 200, address: "12
 
 invoice2.print_out
 bill_of_lading.print_out
+
+# 3. The Liskov subtitution Principle
+=begin
+    - Ability to replace any instance of a parent class with an instance of its child classes 
+    without a negative side effect. for example       
+=end
+
+require 'date'
+require 'ostruct'
+
+class User 
+    attr_accessor :settings, :email
+    
+    def initialize(email:)
+        @email = email 
+    end
+
+    def set_settings(level:, status:, signed_in:)
+        @settings = OpenStruct.new(
+            level: level,
+            status: status,
+            signed_in: signed_in
+        )
+    end 
+
+    def get_settings
+        @settings
+    end
+end 
+
+class AdminUser < User 
+end
+
+user = User.new(email: "user@test.com")
+user.settings = {
+    level: "Low Security",
+    status: "Live",
+    signed_in: Date.today
+}
+
+admin = AdminUser.new(email: "admin@test.com")
+admin.settings = {
+    level: "Editor",
+    status: "VIP",
+    signed_in: Date.today
+}
+
+@user_database = [user, admin]
+
+def signed_in_today?
+    @user_database.each do |user|
+        if user.settings[:signed_in] == Date.today
+            puts "#{user.email} signed in today"
+        end 
+    end
+end
+
+signed_in_today?
