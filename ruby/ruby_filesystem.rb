76929f1ad0f1934f032170e7879369729019bc30
zimaster <symokevo@gmail.com>
2023-04-20 18:35:04 +0300

    learn to read file contents using the ruby File class

--- ruby/ruby_filesystem.rb
+++ ruby/ruby_filesystem.rb
@@ -15,3 +15,20 @@ File.open("ruby-files/teams.txt", 'w+') {
 file_to_save = File.new("ruby-files/otherteams.txt", 'w+')
 file_to_save.puts("A's, Diamongbacks, Mariners, Marlins")
 file_to_save.close
+
+# Reading files into a program using the File class
+teams = File.read("ruby-files/teams.txt")
+puts teams.split
+p teams.split(', ')
+
+# reading form multiple files at the same time
+teams_A = File.read("ruby-files/teams.txt")
+teams_B = File.read("ruby-files/otherteams.txt")
+
+# combine the arrays into one
+main_team = teams_A.split(', ') + teams_B.split(', ')
+
+# convert the main_team to uppercase
+main_team.each { |team| p team.upcase }
+
+
