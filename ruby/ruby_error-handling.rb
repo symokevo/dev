75ec50612f2b2e9c02e3c2e033a032aa12769101
zimaster <symokevo@gmail.com>
2023-04-21 15:24:14 +0300

    code example on error handling with ruby

--- ruby/ruby_error-handling.rb
+++ ruby/ruby_error-handling.rb
@@ -19,4 +19,17 @@ rescue StandardError => e
     puts "Error occurred: #{e}"
 end 
 
+# Custom error logger
+def error_logger (e)
+    File.open('ruby-files/error_log.txt', 'a') do |file|
+        file.puts e 
+    end
+end 
+
+# the main code
+begin
+    puts nil / 10
+rescue StandardError => e
+    error_logger("Error: #{e} at #{Time.now}")    
+end
 
