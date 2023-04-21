95b2e229e76b35ebe84fa7b9d49f834f5e68420c
zimaster <symokevo@gmail.com>
2023-04-21 16:00:16 +0300

    use regex to validate an email entry

---
+++ ruby/regular-expressions.rb
@@ -0,0 +1,36 @@
+string = "The quick 12 brown foxes jumped over  10 lazy dogs"
+# check letter 
+p string =~ /o/
+p string =~ /foxes/
+p string =~ /quick/
+
+p string =~ /z/ ? "valid" : "invalid" 
+# expression is case sensitve. meaning /Z/ will be invalid
+# use /z/i to make it insesitive
+# the above line is a simpler version of:
+if string =~ /z/
+    p "Valid"
+else
+    p "Invalid"
+end
+
+# anything between // is a regular expression
+
+# return all the integer values from the string value
+p string.to_enum(:scan, /\d+/).map {Regexp.last_match}
+
+# Regex code example 
+# Validating email
+VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i 
+# the expression above accepts something@domain.extension
+
+# verifier method
+def is_valid_email? email 
+    email =~ VALID_EMAIL_REGEX
+end
+
+p is_valid_email?("symo@kevo.com") ? "valid": "Invalid"
+p is_valid_email?("symokevo.com") ? "valid": "Invalid"
+p is_valid_email?("symo.r@kevo.com") ? "valid": "Invalid"
+p is_valid_email?("symo@kevo") ? "valid": "Invalid"
+
