a71cdc659a38e2101135011a11219c94a031c147
zimaster <symokevo@gmail.com>
2023-04-24 07:56:14 +0300

    use grep to serach an array

--- ruby/grep_search.rb
+++ ruby/grep_search.rb
@@ -5,3 +5,8 @@
 arr = [1, 3, 2, 12, 1, 2, 3]
 p arr.grep(1)
 p arr.grep(100) # returns empty array
+
+# how to use grep instead of the select and map methods
+arr = ['hey.rb', 'there.rb', 'index.html']
+p arr.select { |x| x=~ /\.rb/ }.map{ |x| x[0..-4]} # using select
+arr.grep(/(.*)\.rb/) {$1}
