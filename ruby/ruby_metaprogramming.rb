b4b4a37ce308556304c9a5918ea6ef7ebbb3e5e6
zimaster <symokevo@gmail.com>
2023-04-30 14:42:55 +0300

    Use methods to expand class functionality
    Reused the String methods' functions to implement more functionalities of the class

--- ruby/ruby_metaprogramming.rb
+++ ruby/ruby_metaprogramming.rb
@@ -18,3 +18,23 @@ end
 
 p Baseball.new.swing
 
+# How to use metaprogramming for opening and custmizing the String class
+class String
+  def censor(bad_word)
+    self.gsub! "#{bad_word}", "CENSORED"
+  end
+end
+
+p "The bunny was in trouble with the king's bunny".censor('bunny')
+p "The bunny was in trouble with the king's bunny".size
+# create a more explicit method name
+
+class String
+
+  def num_of_chars
+    size
+  end
+end
+
+p "The bunny was in trouble with the king's bunny".num_of_chars
+
