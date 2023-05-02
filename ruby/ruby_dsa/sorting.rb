e7bc28899683ab98e8e2148e61897b069f33aeeb
zimaster <symokevo@gmail.com>
2023-05-02 13:21:27 +0300

    Create short notes on sorting algorithms

--- ruby/ruby_dsa/sorting.rb
+++ ruby/ruby_dsa/sorting.rb
@@ -1,3 +1,28 @@
 # Introduction to SORTING algorithms
 a = [1, 5, 6, 6, 13, 213, 65, 7, 43, 90]
 p a.sort
+
+=begin
+Bubble sort
+***********
+- starts from the leftmost value and keeps moving to the right
+- if next value in comparison > previous value, it is replaced by a higher value and the sorting continues
+
+
+Quick Sort
+**********
+- widely used
+- uses the devide and conquer concept
+- picks the pivot point and divides the array into two groups... ie
+- one below the pivot point and the other above
+
+
+Merge sort
+**********
+- uses the devide and conquer concept as well
+- divides the entire collection into subsectons, sort them and remerge it with the next subsection
+
+=end
+
+
+
