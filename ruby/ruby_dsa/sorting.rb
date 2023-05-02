30f75e17a2df2e2774ba282b3b3593fd02b48d3b
zimaster <symokevo@gmail.com>
2023-05-02 19:27:28 +0300

    Implement bubble sort algorithm

--- ruby/ruby_dsa/sorting.rb
+++ ruby/ruby_dsa/sorting.rb
@@ -1,6 +1,6 @@
 # Introduction to SORTING algorithms
-a = [1, 5, 6, 6, 13, 213, 65, 7, 43, 90]
-p a.sort
+# a = [1, 5, 6, 6, 13, 213, 65, 7, 43, 90]
+# p a.sort
 
 =begin
 Bubble sort
@@ -24,5 +24,24 @@ Merge sort
 
 =end
 
+# Implementing the bubble sort algorithm
+
+def bubble_sort(array)
+  n = array.length
+  loop do
+    swapped = false
+    (n-1).times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+    break if not swapped
+  end
+  array
+end
+
+a = [1, 5, 6, 6, 13, 213, 65, 7, 43, 90]
+p bubble_sort(a)
 
 
