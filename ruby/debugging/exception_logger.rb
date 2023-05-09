647ef3b9c824cd64c7368e9403334db40bf5f12b
zimaster <symokevo@gmail.com>
2023-05-09 07:59:18 +0300

    Refactor exception_logger file

---
+++ ruby/debugging/exception_logger.rb
@@ -0,0 +1,11 @@
+require 'logger'
+logger = Logger.new(STDOUT)
+logger.info("Starting Application")
+
+begin
+  0 / 0
+rescue StandardError => e
+  logger.error(e.message)
+end
+
+logger.info("Finishing Application")
