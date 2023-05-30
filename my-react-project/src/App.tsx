4bf113229e507a3932a19a49683678d6deb7617f
Simon Rabuogi <symokevo@gmail.com>
2023-05-30 08:21:02 +0300

    create interfaces for type cheking with Typescripy

--- my-react-project/src/App.tsx
+++ my-react-project/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import reference to the ItemList component
+import { ItemsListComponent } from './components/ItemsList.components'
+import { ItemInterface } from './models/items/Item.interface';
 
+// mock data
+const items: ItemInterface[] = [{
+  id:1,
+  name: 'Item 1',
+  selected: false
+}, {
+  id: 2,
+  name: 'Item 2',
+  selected: false
+}, {
+  id: 3,
+  name: 'Item 3',
+  selected: false
+}]
+
+// component
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <ItemsListComponent items={items}/>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- my-react-project/src/components/ItemsList.components.tsx
+++ my-react-project/src/components/ItemsList.components.tsx
@@ -1,8 +1,9 @@
 // Using const of type React.FC
 import React from 'react';
+import { ItemInterface } from '../models/items/Item.interface';
 
 export const ItemsListComponent: React.FC<{
-    items: any[]
+    items: ItemInterface[]
 }> = (props) => {
 
     return (
